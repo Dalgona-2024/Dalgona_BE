1abeed07ece25352806f6730fcb32195391745bf
sim <simyechan87@gmail.com>
2024-07-16 22:49:00 +0900

    로그인, 회원가입 완성

--- signup_login/src/main/java/com/example/signup_login/domain/user/entity/User.java
+++ signup_login/src/main/java/com/example/signup_login/domain/user/entity/User.java
@@ -1,15 +1,12 @@
 package com.example.signup_login.domain.user.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
+
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
--- signup_login/src/main/java/com/example/signup_login/global/property/redis/RedisProperties.java
+++ signup_login/src/main/java/com/example/signup_login/global/property/redis/RedisProperties.java
@@ -3,13 +3,45 @@ package com.example.signup_login.global.property.redis;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
+@Component
 @Getter
-@RequiredArgsConstructor
 @ConfigurationProperties(prefix = "spring.redis")
 public class RedisProperties {
-    private final String host;
-    private final int port;
-    private final String password;
 
+    private String host;
+    private int port;
+    private String password;
+
+    // Spring Boot를 위해 리플렉션을 통한 인스턴스화를 지원하는 기본 생성자
+    public RedisProperties() {
+    }
+
+    // 모든 필드에 대한 게터와 세터
+    // 주의: Lombok의 @Getter는 모든 필드에 대한 게터를 생성합니다
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
